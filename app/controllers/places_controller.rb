6a18a7c5d9a1dc4f15fb1259c76d8f80fb8cd0dd
Greta Wall <greta.wall@kellogg.northwestern.edu>
2024-06-05 08:01:53 +0000

    m

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,20 +11,12 @@ class PlacesController < ApplicationController
       end
 
   def new
-    @user = User.find_by({ "id" => session["user_id"] })
   end 
 
   def create
-    @user = User.find_by({ "id" => session["user_id"]  })
-  
-    if @user
-      @place = Place.find_by({ "id" => params["id"] })
-      @place = Place.new
-      @place["name"] = params["name"]
-      @place.save
-    else
-      flash["notice"] = "Please login first."
-      redirect_to "/login"
-    end
+    @place = Place.new
+    @place["name"] = params["name"]
+    @place.save
+    redirect_to "/places"
   end
 end
