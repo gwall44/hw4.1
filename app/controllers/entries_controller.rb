2119fca8d63139c6f258cf1e50580ccbdb28a085
Greta Wall <greta.wall@kellogg.northwestern.edu>
2024-06-05 04:18:25 +0000

    c6

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,17 +1,6 @@
 class EntriesController < ApplicationController
   before_action :current_user
 
-  def index
-    @user = User.find_by({ "id" => session["user_id"] })
-    if @user
-      @entries = @user.entries
-    else
-      flash["notice"] = "Please login first."
-      redirect_to "/login"
-    end
-  end
-  
-
   def new
     @user = User.find_by({ "id" => session["user_id"] })
   end
@@ -20,14 +9,18 @@ class EntriesController < ApplicationController
     @user = User.find_by({ "id" => session["user_id"] })
     if @user != nil
       @entry = Entry.new
+      @entry["title"] = params["title"]
       @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
       @entry.uploaded_image.attach(params["uploaded_image"])
       @entry["user_id"] = @user["id"]
+      @entry["place_id"] = @user["place_id"]
       @entry.save
     else
       flash["notice"] = "Please login first."
     end
-    redirect_to "/entries"
+    redirect_to "/places/#{@entry["place_id"]}"
   end
+  
 
 end
